54ed8fd7121576573dbccb41aaddb653d8c0718c
Your Name <you@example.com>
2022-08-23 12:43:55 -0500

    Tarea Craecion de Clase Empleado - Alrjandra

---
+++ YAWER/src/test/java/com/example/yawer/YawerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yawer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YawerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
